8e6ba9d6edd03fa86a294455aad00145efa42ea1
Vaguinaldo <vaguinaldo@outlook.com>
2022-03-29 20:59:20 -0300

    criação do metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,11 @@
-class NewPessoa():
-    ...
+class Pessoa:
+     def cumprimentar(self):
+         return f'Ola{id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(p.cumprimentar())
+    print(id(p))
+    print(Pessoa.cumprimentar(p))
 
