0568f6b4b2f75fc217e9591468a8a05307b3d448
Vaguinaldo <vaguinaldo@outlook.com>
2022-03-29 21:15:28 -0300

    criação dos atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,11 @@
 class Pessoa:
-     def cumprimentar(self):
-         return f'Ola{id(self)}'
+    def __init__(self, nome=None, idade=29):
+        #atalho alt + enter cria os atributos
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Ola{id(self)}'
 
 
 if __name__ == '__main__':
@@ -8,4 +13,6 @@ if __name__ == '__main__':
     print(p.cumprimentar())
     print(id(p))
     print(Pessoa.cumprimentar(p))
+    p.nome = 'vaguinaldo'
+    print(p.nome)
 
