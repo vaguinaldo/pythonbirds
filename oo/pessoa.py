5c55607fb12ddd1cb2d7e6d0a3bb7fdeeba91734
Vaguinaldo <vaguinaldo@outlook.com>
2022-03-29 22:15:02 -0300

    criação dos atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    #atributo de classe
+    olhos=2
     def __init__(self, *filhos, nome=None, idade=29):
         #atalho alt + enter cria os atributos
         self.filhos = list(filhos)
@@ -14,5 +16,13 @@ if __name__ == '__main__':
     joao = Pessoa(vagui, nome='joao')
     print(vagui.idade)
     print(joao.filhos)
+    print(joao.olhos)
     for filho in joao.filhos:
         print(filho.nome)
+
+    vagui.sobrenome='pereira'
+    #para deleter uma atriburo de um objeto --> del
+    # del vaguinaldo.sobrenome
+
+    print(vagui.__dict__)
+    print(id(vagui.olhos))
